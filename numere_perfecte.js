8409c6139c6150141f75baa2e70809ea9d20fad3
SzilagyiAndras <51080832+SzilagyiAndras@users.noreply.github.com>
2019-05-27 15:41:40 +0300

    Update numere_perfecte.js

--- numere_perfecte.js
+++ numere_perfecte.js
@@ -2,7 +2,7 @@ var i=0; //contor
 var sumadivizori=0; 
 var numar=0;
  function numerePerfecte(){
-	 for(numar=0;numar<100000;numar++){
+	 for(numar=0;numar<10000000;numar++){
 		 sumadivizori=0;
 		 for(i=0;i<=numar/2;i++){
 			 if(numar %i==0){
